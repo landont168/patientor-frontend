aa27bc97a0d327c2e1a7d43d21bb6de2f568386c
Landon Trinh <111808279+landont168@users.noreply.github.com>
2024-12-26 14:57:33 -0500

    add: display patient entries on fe

--- src/components/PatientPage.tsx
+++ src/components/PatientPage.tsx
@@ -7,7 +7,13 @@ import FemaleIcon from "@mui/icons-material/Female";
 import TransgenderIcon from "@mui/icons-material/Transgender";
 
 import patientService from "../services/patients";
-import { Patient } from "../types";
+import {
+  Patient,
+  Entry,
+  HospitalEntry,
+  HealthCheckEntry,
+  OccupationalHealthcareEntry,
+} from "../types";
 
 const PatientPage = () => {
   const [patient, setPatient] = useState<Patient>();
@@ -37,6 +43,83 @@ const PatientPage = () => {
       </h2>
       <div>ssn: {patient.ssn}</div>
       <div>occupation: {patient.occupation}</div>
+      <h2>entries</h2>
+      {patient.entries.map((entry) => (
+        <div key={entry.id}>
+          <EntryDetails entry={entry} />
+          <br />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+const EntryDetails = ({ entry }: { entry: Entry }) => {
+  switch (entry.type) {
+    case "Hospital":
+      return <HospitalEntryDetails entry={entry} />;
+    case "OccupationalHealthcare":
+      return <OccupationalHealthcareEntryDetails entry={entry} />;
+    case "HealthCheck":
+      return <HealthCheckEntryDetails entry={entry} />;
+    default:
+      return assertNever(entry);
+  }
+};
+
+const HospitalEntryDetails = ({ entry }: { entry: HospitalEntry }) => {
+  return (
+    <div>
+      <div>{entry.date}</div>
+      <div>
+        <em>{entry.description}</em>
+      </div>
+      <div>
+        <strong>Discharge:</strong> {entry.discharge.date}:{" "}
+        {entry.discharge.criteria}
+      </div>
+      <div>diagnose by: {entry.specialist}</div>
+    </div>
+  );
+};
+
+const HealthCheckEntryDetails = ({ entry }: { entry: HealthCheckEntry }) => {
+  return (
+    <div>
+      <div>{entry.date}</div>
+      <div>
+        <em>{entry.description}</em>
+      </div>
+      <div>health check rating: {entry.healthCheckRating}</div>
+      <div>diagnose by: {entry.specialist}</div>
+    </div>
+  );
+};
+
+const OccupationalHealthcareEntryDetails = ({
+  entry,
+}: {
+  entry: OccupationalHealthcareEntry;
+}) => {
+  return (
+    <div>
+      <div>{entry.date}</div>
+      <div>
+        <em>{entry.description}</em>
+      </div>
+      <div>employer: {entry.employerName}</div>
+      {entry.sickLeave && (
+        <div>
+          sick leave: {entry.sickLeave.startDate} - {entry.sickLeave.endDate}
+        </div>
+      )}
+      <div>diagnose by: {entry.specialist}</div>
     </div>
   );
 };
